11facde3b1140d632ce13c8ea3f0b0ab136b635c
ZiHang Fu <2364369295@qq.com>
2024-12-11 21:20:24 +0800

    init

---
+++ src/test/java/com/example/intelligenteducationtrackingsystem/IntelligentEducationTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intelligenteducationtrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentEducationTrackingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

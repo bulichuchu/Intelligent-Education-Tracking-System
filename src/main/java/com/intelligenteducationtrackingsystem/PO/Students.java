8d2ee30b20a08962c2cdfb59a8f6ec9f190fafa6
86188 <1164904649@qq.com>
2024-12-12 18:16:55 +0800

    Fix issue with Students.java

--- src/main/java/com/intelligenteducationtrackingsystem/PO/Students.java
+++ src/main/java/com/intelligenteducationtrackingsystem/PO/Students.java
@@ -9,83 +9,83 @@ import javax.persistence.Table;
 public class Students {
 
     @Id
-    private String StudentId; // 对应数据表中的 studentid 列，作为主键
+    private String studentId; // 对应数据表中的 studentid 列，作为主键
 
-    private String StudentName; // 对应数据表中的 studentname 列，存储学生姓名
-    private String ClassId; // 对应数据表中的 classid 列，代表所在班级编号
-    private String Sex; // 对应数据表中的 sex 列，标识学生性别
-    private int Age; // 对应数据表中的 age 列，记录学生年龄
-    private String Address; // 对应数据表中的 address 列，存放学生地址
-    private String PhoneNumber; // 对应数据表中的 phonenumber 列，为学生电话号码
+    private String studentName; // 对应数据表中的 studentname 列，存储学生姓名
+    private String classId; // 对应数据表中的 classid 列，代表所在班级编号
+    private String sex; // 对应数据表中的 sex 列，标识学生性别
+    private int age; // 对应数据表中的 age 列，记录学生年龄
+    private String address; // 对应数据表中的 address 列，存放学生地址
+    private String phoneNumber; // 对应数据表中的 phonenumber 列，为学生电话号码
 
     // 默认构造函数
     public Students() {
     }
 
     public Students(String studentId, String studentName, String classId, String sex, int age, String address, String phoneNumber) {
-        this.StudentId = studentId;
-        this.StudentName = studentName;
-        this.ClassId = classId;
-        this.Sex = sex;
-        this.Age = age;
-        this.Address = address;
-        this.PhoneNumber = phoneNumber;
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.classId = classId;
+        this.sex = sex;
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getStudentId() {
-        return StudentId;
+        return studentId;
     }
 
     public void setStudentId(String studentId) {
-        this.StudentId = studentId;
+        this.studentId = studentId;
     }
 
     public String getStudentName() {
-        return StudentName;
+        return studentName;
     }
 
     public void setStudentName(String studentName) {
-        this.StudentName = studentName;
+        this.studentName = studentName;
     }
 
     public String getClassId() {
-        return ClassId;
+        return classId;
     }
 
     public void setClassId(String classId) {
-        this.ClassId = classId;
+        this.classId = classId;
     }
 
     public String getSex() {
-        return Sex;
+        return sex;
     }
 
     public void setSex(String sex) {
-        this.Sex = sex;
+        this.sex = sex;
     }
 
     public int getAge() {
-        return Age;
+        return age;
     }
 
     public void setAge(int age) {
-        this.Age = age;
+        this.age = age;
     }
 
     public String getAddress() {
-        return Address;
+        return address;
     }
 
     public void setAddress(String address) {
-        this.Address = address;
+        this.address = address;
     }
 
     public String getPhoneNumber() {
-        return PhoneNumber;
+        return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.PhoneNumber = phoneNumber;
+        this.phoneNumber = phoneNumber;
     }
 
     @Override

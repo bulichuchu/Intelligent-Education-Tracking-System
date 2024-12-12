18517792f9e3b9234687ea0193dcf85ea6561800
XWD <1098996870@qq.com>
2024-12-12 13:06:38 +0800

    删除example包，重命名

--- src/test/java/com/intelligenteducationtrackingsystem/IntelligentEducationTrackingSystemApplicationTests.java
+++ src/test/java/com/intelligenteducationtrackingsystem/IntelligentEducationTrackingSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.intelligenteducationtrackingsystem;
+package com.intelligenteducationtrackingsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
